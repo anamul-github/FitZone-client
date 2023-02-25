0da75a671bffe4878009dbff69d3f81f3b83f743
Anamul10 <anamul.hasan@mnsu.edu>
2023-02-25 11:53:18 -0500

    added services from mongoDB

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,25 @@
 import Lottie from 'lottie-react';
 import React from 'react';
+import Services from '../Services/Services';
 import push from './../../assets/133718-boy-doing-bench-press.json';
-
 
 
 const Home = () => {
     return (
-        <div className="bg-[url('https://i.ibb.co/M80NsLX/gradient-background.png')] flex flex-col sm:flex-row justify-center items-center lg:h-screen">
-            <div className="w-full sm:w-1/2 p-4">
-                <Lottie className="w-full h-auto" loop={true} animationData={push} />
-            </div>
-            <div className="w-full sm:w-1/2 pr-10">
-                <h2 className="text-5xl font-bold mb-4 text-center lg:pr-10">FitZone</h2>
-                <p className="text-2xl font-bold mb-4 py-2 lg:pr-10 text-center">Transform Your Body, Transform Your Life. Join Us Today and Achieve Your Fitness Goals!</p>
-                <div className='flex justify-center lg:pr-10'>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Learn More</button>
+        <div>
+            <div className="bg-[url('https://i.ibb.co/M80NsLX/gradient-background.png')] flex flex-col sm:flex-row justify-center items-center lg:h-screen">
+                <div className="w-full sm:w-1/2 p-4">
+                    <Lottie className="w-full h-auto" loop={true} animationData={push} />
+                </div>
+                <div className="w-full sm:w-1/2 lg:pr-20">
+                    <h2 className="text-5xl font-bold mb-4 text-center lg:pr-10 text-black-900">FitZone</h2>
+                    <p className="text-2xl font-bold mb-4 py-2 lg:pr-10 text-center">Transform Your Body, Transform Your Life. Join Us Today and Achieve Your Fitness Goals!</p>
+                    <div className='flex justify-center lg:pr-10'>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Learn More</button>
+                    </div>
                 </div>
             </div>
+            <Services></Services>
         </div>
     );
 };
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import { Link } from 'react-router-dom';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
+
     return (
         <div>
-            <h2>This is Services page</h2>
+            <h2 className='text-4xl text-center font-bold'>Services</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                {
+                    services.map(service => (
+                        <div className="mx-auto card w-96 bg-base-100 shadow-xl" key={service._id}>
+                            <div className="relative">
+                                <PhotoProvider>
+                                    <PhotoView src={service.img}>
+                                        <img className="w-full h-auto object-cover" src={service.img} alt={service.name} />
+                                    </PhotoView>
+                                </PhotoProvider>
+                            </div>
+                            <div className="p-4">
+                                <h2 className="text-xl font-medium mb-2">{service.name}</h2>
+                                <p className="text-gray-700 mb-2">${service.price}</p>
+                                <p className="text-gray-600">{service.description}</p>
+                                <Link to={`/serviceDetails/${service._id}`} className="mt-4 inline-block">
+                                    <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">
+                                        View Details
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    ))
+                }
+                <div className='mx-auto'>
+                    <Link to='/allServices'><button className='btn-success px-5 mb-5'>See All</button>
+                    </Link>
+                </div>
+
+            </div>
         </div>
     );
 };
